c9763424039119984d5525ac65af4c3cf45fb393
AutstcDragonite <autisticdragonite@gmail.com>
2023-12-05 23:32:47 +0700

    added dummy solution, fill in code when ready

---
+++ Pori Translated/PoriTranslatedMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Il2Cpp;
+using Il2CppGrdk;
+using MelonLoader;
+using UnityEngine.SceneManagement;
+using UniverseLib;
+
+namespace Pori_Translated
+{
+    public class PoriTranslatedMain : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("hiii :3");
+        }
+    }
+}
